8f856537226b18518fa5871e3d3710b48ae03edc
Mikael <kansimies39@gmail.com>
2024-01-08 19:41:02 +0200

    Testijuttu

---
+++ testing/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b, c, avg;
+    cout << "Anna kolme lukua, niin lasketaan niiden keskiarvo" << endl;
+    cin >> a >> b >> c;
+
+
+    avg = (a + b + c)/3;
+
+    cout << "Keskiarvo on: " << avg << endl;
+
+
+    return 0;
+}
